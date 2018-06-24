51db5ca6af9550c8199fa29d176a97f66a685c30
dogbeide <boyowao@gmail.com>
2018-06-23 23:51:08 -0400

    Add /signin and /register endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,57 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 
 const app = express();
 
+const db = {
+  users: [
+    {
+      id: '1',
+      name: 'Jon',
+      email: 'joncagley@gmail.com',
+      password: 'lavaguns',
+      entries: 0,
+      createdAt: new Date()
+    },
+    {
+      id: '2',
+      name: 'Chirag',
+      email: 'chiraggupta@gmail.com',
+      password: 'brother',
+      entries: 0,
+      createdAt: new Date()
+    },
+  ]
+}
+
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
-  res.send('yes')
-})
+  res.send(db.users)
+});
+
+app.post('/signin', (req, res) => {
+  if (req.body.email === db.users[0].email &&
+      req.body.password === db.users[0].password) {
+    res.json('success');
+  } else {
+    res.status(400).json('username or password incorrect');
+  }
+});
+
+app.post('/register', (req, res) => {
+  const { name, email, password } = req.body;
+  db.users.push({
+    id: db.users.length + 1,
+    name,
+    email,
+    password,
+    entries: 0,
+    createdAt: new Date()
+  });
+  res.json(db.users[db.users.length-1]);
+});
 
 app.listen(3000, () => {
   console.log('server is running on port 3000');
