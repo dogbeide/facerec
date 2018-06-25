e7f4d2fcbb6560839a3d7eccba8efe45c887c121
dogbeide <boyowao@gmail.com>
2018-06-24 23:19:49 -0400

    Connect /image endpoint to postgresql

--- backend/server.js
+++ backend/server.js
@@ -64,17 +64,14 @@ app.get('/profile/:id', (req, res) => {
 
 app.put('/image', (req, res) => {
   const { id } = req.body;
-  let found = false;
-  dbFake.users.forEach(user => {
-    if (user.id === id) {
-      user.entries++;
-      res.json(user.entries);
-      found = true;
-    }
-  });
-  if (!found) {
-    res.status(404).json('user not found');
-  }
+  db('users')
+    .where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
 });
 
 app.listen(port, () => {
